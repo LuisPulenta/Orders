eb848068928a7562795d3ec3455a5a31b036c8f2
LuisPulenta <luisalbertonu@gmail.com>
2024-03-01 12:38:38 -0300

    GenericList

---
+++ Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        private List<Country>? Countries;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
--- Orders.Frontend/Program.cs
+++ Orders.Frontend/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7225//") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7057/") });
 builder.Services.AddScoped<IRepository, Repository>();
 await builder.Build().RunAsync();
