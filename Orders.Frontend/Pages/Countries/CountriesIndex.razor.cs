76b204ef9ca1f9a55185b1a1f6c0a76e7d1495fd
LuisPulenta <luisalbertonu@gmail.com>
2024-03-08 14:25:03 -0300

    DeleteCountry

--- Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,21 +1,81 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
 using Orders.Frontend.Repositories;
 using Orders.Shared.Entities;
+using System.Net;
 
 namespace Orders.Frontend.Pages.Countries
 {
     public partial class CountriesIndex
     {
-        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private IRepository repository { get; set; } = null!;
 
         private List<Country>? Countries;
 
+        //-----------------------------------------------------------------------------------
         protected async override Task OnInitializedAsync()
         {
-            await base.OnInitializedAsync();
+            await LoadAsync();
+        }
 
-            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
-            Countries = responseHttp.Response;
+        //-----------------------------------------------------------------------------------
+        private async Task LoadAsync()
+        {
+            var responseHppt = await repository.GetAsync<List<Country>>("api/countries");
+            if (responseHppt.Error)
+            {
+                var message = await responseHppt.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Countries = responseHppt.Response!;
+        }
+
+        //-----------------------------------------------------------------------------------
+        private async Task DeleteAsync(Country country)
+        {
+            var result = await sweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = $"¿Esta seguro que quieres borrar el país: {country.Name}?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+
+            if (confirm)
+            {
+                return;
+            }
+
+            var responseHttp = await repository.DeleteAsync($"api/countries/{country.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    var mensajeError = await responseHttp.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                }
+                return;
+            }
+            
+            await LoadAsync();
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.Center,
+                ShowConfirmButton = true,
+                Timer = 3000,
+                Background = "LightSkyBlue",
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con éxito.");
         }
     }
 }
---
+++ Orders.Frontend/Pages/Countries/CountryEdit.razor.cs
@@ -0,0 +1,70 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+using System.Net;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountryEdit
+    {
+        private Country? country;
+        private CountryForm? countryForm;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [EditorRequired,Parameter] public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<Country>($"api/countries/{Id}");
+
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("countries");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                country = responseHttp.Response;
+            }
+        }
+
+        private async Task EditAsync()
+        {
+            var responseHTTP = await Repository.PutAsync("api/countries", country);
+
+            if (responseHTTP.Error)
+            {
+                var mensajeError = await responseHTTP.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                return;
+            }
+
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.Center,
+                ShowConfirmButton = true,
+                Timer = 3000,
+                Background = "LightSkyBlue",
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+
+        private void Return()
+        {
+            countryForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("countries");
+        }
+    }
+}
