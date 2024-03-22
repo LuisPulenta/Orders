3e86af41fa97c4bd0e8bddf66a6d85c9e3451278
LuisPulenta <luisalbertonu@gmail.com>
2024-03-22 15:41:41 -0300

    CountryRepository

--- Orders.Backend/Data/SeedDb.cs
+++ Orders.Backend/Data/SeedDb.cs
@@ -22,12 +22,69 @@ namespace Orders.Backend.Data
         {
             if (!_context.Countries.Any())
             {
-                _context.Countries.Add(new Country { Name = "Colombia" });
-                _context.Countries.Add(new Country { Name = "Estados Unidos" });
+                _context.Countries.Add(new Country
+                {
+                    Name = "Colombia",
+                    States = new List<State>()
+            {
+                new State()
+                {
+                    Name = "Antioquia",
+                    Cities = new List<City>() {
+                        new () { Name = "Medellín" },
+                        new () { Name = "Itagüí" },
+                        new () { Name = "Envigado" },
+                        new () { Name = "Bello" },
+                        new () { Name = "Rionegro" },
+                    }
+                },
+                new State()
+                {
+                    Name = "Bogotá",
+                    Cities = new List<City>() {
+                        new () { Name = "Usaquen" },
+                        new () { Name = "Champinero" },
+                        new () { Name = "Santa fe" },
+                        new () { Name = "Useme" },
+                        new () { Name = "Bosa" },
+                    }
+                },
+            }
+                });
+                _context.Countries.Add(new Country
+                {
+                    Name = "Estados Unidos",
+                    States =
+            [
+                new State()
+                {
+                    Name = "Florida",
+                    Cities = [
+                        new () { Name = "Orlando" },
+                        new () { Name = "Miami" },
+                        new () { Name = "Tampa" },
+                        new () { Name = "Fort Lauderdale" },
+                        new () { Name = "Key West" },
+                    ]
+                },
+                new State()
+                {
+                    Name = "Texas",
+                    Cities = [
+                        new () { Name = "Houston" },
+                        new () { Name = "San Antonio" },
+                        new () { Name = "Dallas" },
+                        new () { Name = "Austin" },
+                        new () { Name = "El Paso" },
+                    ]
+                },
+            ]
+                });
             }
 
             await _context.SaveChangesAsync();
         }
+
 
         private async Task CheckCategoriesAsync()
         {
