6e80ed450b03b2a01a3d724eefec6ab9bd74e582
LuisPulenta <luisalbertonu@gmail.com>
2024-04-13 18:01:01 -0300

    Video 40

---
+++ Orders.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Orders.Frontend.AuthenticationProviders
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(300);
+            
+            var anonimous = new ClaimsIdentity();
+            
+            var user = new ClaimsIdentity(authenticationType: "test");
+
+            var admin = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Luis"),
+                new Claim("LastName", "Núñez"),
+                new Claim(ClaimTypes.Name, "luis@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+
+            },
+            authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
