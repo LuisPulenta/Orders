b83e0dfa42cc2ca67b3a5085ba10de3d64b4b69c
LuisPulenta <luisalbertonu@gmail.com>
2024-04-14 12:45:18 -0300

    Video 42

--- Orders.Backend/Repositories/Implementations/UsersRepository.cs
+++ Orders.Backend/Repositories/Implementations/UsersRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
 using Orders.Backend.Repositories.Interfaces;
+using Orders.Shared.DTOs;
 using Orders.Shared.Entities;
 
 namespace Orders.Backend.Repositories.Implementations
@@ -11,24 +12,29 @@ namespace Orders.Backend.Repositories.Implementations
         private readonly DataContext _context;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public UsersRepository(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public UsersRepository(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
         {
             _context = context;
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
         }
 
+        //-------------------------------------------------------------------------------------------
         public async Task<IdentityResult> AddUserAsync(User user, string password)
         {
             return await _userManager.CreateAsync(user, password);
         }
 
+        //-------------------------------------------------------------------------------------------
         public async Task AddUserToRoleAsync(User user, string roleName)
         {
             await _userManager.AddToRoleAsync(user, roleName);
         }
 
+        //-------------------------------------------------------------------------------------------
         public async Task CheckRoleAsync(string roleName)
         {
             var roleExists = await _roleManager.RoleExistsAsync(roleName);
@@ -41,6 +47,7 @@ namespace Orders.Backend.Repositories.Implementations
             }
         }
 
+        //-------------------------------------------------------------------------------------------
         public async Task<User> GetUserAsync(string email)
         {
             var user = await _context.Users
@@ -51,9 +58,23 @@ namespace Orders.Backend.Repositories.Implementations
             return user!;
         }
 
+        //-------------------------------------------------------------------------------------------
         public async Task<bool> IsUserInRoleAsync(User user, string roleName)
         {
             return await _userManager.IsInRoleAsync(user, roleName);
         }
+
+        //-------------------------------------------------------------------------------------------
+        public async Task<SignInResult> LoginAsync(LoginDTO model)
+        {
+            return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+        }
+
+        //-------------------------------------------------------------------------------------------
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
     }
 }
