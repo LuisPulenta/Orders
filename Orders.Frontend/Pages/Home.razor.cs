3808ec09aea7bf0571a589d24ef11865d897e565
LuisPulenta <luisalbertonu@gmail.com>
2024-05-24 15:09:07 -0300

    Category Filter

--- Orders.Frontend/Pages/Home.razor.cs
+++ Orders.Frontend/Pages/Home.razor.cs
@@ -15,8 +15,12 @@ namespace Orders.Frontend.Pages
         private int totalPages;
         private int counter = 0;
         private bool isAuthenticated;
+        private string allCategories = "all_categories_list";
 
         public List<Product>? Products { get; set; }
+        public List<Category>? Categories { get; set; }
+        public string CategoryFilter { get; set; } = string.Empty;
+
         [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
         [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
         [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 8;
@@ -42,8 +46,20 @@ namespace Orders.Frontend.Pages
         }
 
         //-----------------------------------------------------------------------------------
-        private async Task LoadAsync(int page = 1)
+        private async Task LoadAsync(int page = 1, string category = "")
         {
+            if (!string.IsNullOrWhiteSpace(category))
+            {
+                if (category == allCategories)
+                {
+                    CategoryFilter = string.Empty;
+                }
+                else
+                {
+                    CategoryFilter = category;
+                }
+            }
+
             if (!string.IsNullOrWhiteSpace(Page))
             {
                 page = Convert.ToInt32(Page);
@@ -66,6 +82,11 @@ namespace Orders.Frontend.Pages
                 url += $"&filter={Filter}";
             }
 
+            if (!string.IsNullOrEmpty(CategoryFilter))
+            {
+                url += $"&CategoryFilter={CategoryFilter}";
+            }
+
             var response = await Repository.GetAsync<List<Product>>(url);
             if (response.Error)
             {
@@ -84,6 +105,11 @@ namespace Orders.Frontend.Pages
             if (!string.IsNullOrEmpty(Filter))
             {
                 url += $"&filter={Filter}";
+            }
+
+            if (!string.IsNullOrEmpty(CategoryFilter))
+            {
+                url += $"&CategoryFilter={CategoryFilter}";
             }
 
             var response = await Repository.GetAsync<int>(url);
@@ -135,6 +161,7 @@ namespace Orders.Frontend.Pages
         {
             await CheckIsAuthenticatedAsync();
             await LoadCounterAsync();
+            await LoadCategoriesAsync();
         }
 
         //-----------------------------------------------------------------------------------
@@ -201,5 +228,19 @@ namespace Orders.Frontend.Pages
             });
             await toast2.FireAsync(icon: SweetAlertIcon.Success, message: "Producto agregado al carro de compras.");
         }
+        
+        //-----------------------------------------------------------------------------------
+        private async Task LoadCategoriesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Category>>("api/categories/combo");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+            }
+
+            Categories = responseHttp.Response;
+        }
+
     }
 }
